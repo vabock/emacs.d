60c12c563fca4648370b764e33609ab7e077b370
Vabock <vabock@gmail.com>
2026-02-04 22:20:39 +0900

    RictyからNotoへ変更

--- init-x.el
+++ init-x.el
@@ -1,1 +1,3 @@
-(my/build-fontset "Inconsolata" 13 "Ricty" '(japanese-jisx0213.2004-1 katakana-jisx0201))
+(my/build-fontset "Inconsolata" 16 "Noto Sans CJK JP" '(japanese-jisx0213.2004-1 katakana-jisx0201) "semi-bold")
+(add-to-list 'face-font-rescale-alist
+             '(".*Noto.*" . 0.9))
